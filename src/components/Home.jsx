1b37a60f9ea9e28c6030d3c8d50c7160c3187811
AVSheard <a_sheard@yahoo.co.uk>
2020-03-16 18:27:01 +0000

    made a request for articles but untestested

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
 /** @format */
 
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Home extends Component {
+	state = { articles: [] };
+
+	retriveArticles = () => {
+		axios
+			.get("https://nc-news-anthony.herokuapp.com/api/articles")
+			.then((res) => {
+				console.log(res.data);
+				this.setState({ students: res.data.students });
+			});
+	};
+
+	componentDidMount() {
+		this.retriveArticles();
+	}
+
 	render() {
 		return (
 			<div>
