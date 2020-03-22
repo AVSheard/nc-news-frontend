5ee290a702c11a31ab25c66f252e56c11677fa82
AVSheard <a_sheard@yahoo.co.uk>
2020-03-22 22:30:29 +0000

    added a get request to article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,22 @@
 /** @format */
 
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Article extends Component {
 	state = { article: {} };
+
+	retriveArticle = (id) => {
+		axios
+			.get(`https://nc-news-anthony.herokuapp.com/api/articles/${id}`)
+			.then((res) => {
+				this.setState({ article: res.data.article });
+			});
+	};
+
+	componentDidMount() {
+		this.retriveArticle();
+	}
 
 	render() {
 		return (
