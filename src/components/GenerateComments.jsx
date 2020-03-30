9d7695d10c966b2b2046a702e683a025860c5059
AVSheard <a_sheard@yahoo.co.uk>
2020-03-30 12:53:55 +0100

    added a popup to apear when the user presses the delete comment button to confirm that they want to delete that comment

--- src/components/GenerateComments.jsx
+++ src/components/GenerateComments.jsx
@@ -2,47 +2,72 @@
 
 import React, { Component } from "react";
 import axios from "axios";
+import { observer } from "mobx-react";
+import { userInfo } from "../stores/userInfo";
+import Modal from "react-modal";
 
-export default class GenerateComments extends Component {
-	state = { comments: [] };
+export default observer(
+	class GenerateComments extends Component {
+		state = { comments: [], open: false, chosenComent: -1 };
 
-	retriveComments = (id) => {
-		axios
-			.get(`https://nc-news-anthony.herokuapp.com/api/articles/${id}/comments`)
-			.then((res) => {
-				this.setState({ comments: res.data.comments });
-			});
-	};
+		retriveComments = (id) => {
+			axios
+				.get(
+					`https://nc-news-anthony.herokuapp.com/api/articles/${id}/comments`
+				)
+				.then((res) => {
+					this.setState({ comments: res.data.comments });
+				});
+		};
 
-	componentDidMount = () => {
-		this.retriveComments(this.props.id);
-	};
+		closePopUp = () => {
+			this.setState({ open: false });
+		};
 
-	handleClick = (event) => {
-		axios
-			.delete(
-				`https://nc-news-anthony.herokuapp.com/api/comments/${event.target.value}`
-			)
-			.then((res) => {
-				console.log(res);
-			});
-	};
+		componentDidMount = () => {
+			this.retriveComments(this.props.id);
+		};
 
-	render() {
-		return (
-			<ul>
-				{this.state.comments.map((comment) => {
-					return (
-						<div key={comment.comment_id}>
-							<h4>{comment.author}:</h4>
-							<h4>{comment.body}</h4>
-							<button onClick={this.handleClick} value={comment.comment_id}>
-								Delete Comment
-							</button>
-						</div>
-					);
-				})}
-			</ul>
-		);
+		handleClick = (event) => {
+			this.closePopUp();
+			axios
+				.delete(
+					`https://nc-news-anthony.herokuapp.com/api/comments/${this.state.chosenComent}`
+				)
+				.then((res) => {
+					this.retriveComments(this.props.id);
+				});
+		};
+
+		openPopUp = (event) => {
+			this.setState({ open: true, chosenComent: event.target.value });
+		};
+
+		render() {
+			return (
+				<>
+					<ul>
+						{this.state.comments.map((comment) => {
+							return (
+								<div key={comment.comment_id}>
+									<h4>{comment.author}:</h4>
+									<h4>{comment.body}</h4>
+									{userInfo.user === comment.author && (
+										<button onClick={this.openPopUp} value={comment.comment_id}>
+											Delete Comment
+										</button>
+									)}
+								</div>
+							);
+						})}
+					</ul>
+					<Modal className="popup" ariaHideApp={false} isOpen={this.state.open}>
+						<p>Are you sure you want to delete this comment?</p>
+						<button onClick={this.closePopUp}>Cancel</button>
+						<button onClick={this.handleClick}>Delete</button>
+					</Modal>
+				</>
+			);
+		}
 	}
-}
+);
